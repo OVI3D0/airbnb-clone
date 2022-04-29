20603a456fb81b4c0e6b50e4b15a7ff1ec9e528d
Michael Oviedo <michaeloviedo2000@gmail.com>
2022-04-29 16:13:12 -0400

    added reviews and review counts

--- src/components/card.js
+++ src/components/card.js
@@ -13,7 +13,7 @@ const Card = (props) => {
             <div className="card mx-5">
                 {badgeText && <button className="btn btn-light card-btn ms-2 mt-2">{badgeText}</button>}
                 <img src={`/images/${props.card.coverImg}`} alt="" className="rounded"/>
-                <p className="pt-1"><i className="fa-solid fa-star"></i>({props.card.rating})<p className="review-grey d-inline ps-1">{props.card.reviewCount} - {props.card.location}</p></p>
+                <p className="pt-1"><i className="fa-solid fa-star"></i>({props.card.stats.rating})<p className="review-grey d-inline ps-1">{props.card.stats.reviewCount} - {props.card.location}</p></p>
                 <p>{props.card.title}</p>
                 <p className="fw-bold">From ${props.card.price} <p className="d-inline fw-normal">/ person</p></p>
                 <p>Open spots: {props.card.openSpots}</p>
