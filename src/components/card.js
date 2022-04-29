2cf79381b48511e6ce2c332796fff13e2a7672b7
Michael Oviedo <michaeloviedo2000@gmail.com>
2022-04-29 15:51:34 -0400

    added more functionality for the badge in the top left corner

--- src/components/card.js
+++ src/components/card.js
@@ -1,10 +1,20 @@
 import '../styles/card.css'
 
 const Card = (props) => {
+    let badgeText, displayprop
+    if(props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    } else {
+        displayprop = "none"
+        badgeText = ""
+    }
+    
     return (
         <>
             <div className="card mx-5">
-                <button className="btn btn-light card-btn ms-2 mt-2" style={{display: props.openSpots ? "none" : "block"}}>SOLD OUT</button>
+                <button className="btn btn-light card-btn ms-2 mt-2" style={{display: `${displayprop}`}}>{badgeText}</button>
                 <img src={`/images/${props.img}`} alt="" className="rounded"/>
                 <p className="pt-1"><i className="fa-solid fa-star"></i>({props.rating})<p className="review-grey d-inline ps-1">{props.reviewCount} - {props.location}</p></p>
                 <p>{props.title}</p>
